9aa733797e3c30ecbbdf07e4adcfb7c253f0cfca
Charles L Flatt <charles@tfhome.us>
2021-02-12 08:52:50 -0700

    Add project files.

---
+++ SolutionReferences/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolutionReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
